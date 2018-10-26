0ce92ce70e6c4011f2df4d0c6479078cd2c57002
yuan hai zhao <zhaoyuanhai66@outlook.com>
2018-10-26 17:59:03 +0800

    修改之前的兼容行bug 兼容ie

---
+++ CCACAWebUI/wwwroot/js/ieload.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+    function isIE() { //ie?
+        if (!!window.ActiveXObject || "ActiveXObject" in window)
+            return true;
+        else
+            return false;
+    }
+
+    function getLink(href) {
+        var link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = href;
+        document.head.appendChild(link);
+    }
+
+    if (isIE()) {
+        getLink("/css/pages/index-ie.css");
+    }
+});
